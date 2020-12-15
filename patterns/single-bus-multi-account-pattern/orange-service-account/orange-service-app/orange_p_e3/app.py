ec15f6bad3fd939f16330a61ca88fe9fc2058ddf
Stephen Liedig <sliedig@amazon.com>
2020-12-16 01:46:20 +0800

    feat: initial commit for single bus, multi-account pattern example

---
+++ patterns/single-bus-multi-account-pattern/orange-service-account/orange-service-app/orange_p_e3/app.py
@@ -0,0 +1,50 @@
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import os
+import json
+import boto3
+
+# pull the devops event bus arn from an environment variable where 
+# DEVOPS_EVENT_BUS_ARN = 'arn:aws:events:us-east-1:[ACCOUNT-B]:event-bus/devops-event-bus'
+EVENT_BUS_ARN = os.environ['DEVOPS_EVENT_BUS_ARN'] 
+
+# Create EventBridge client
+events = boto3.client('events')
+
+def lambda_handler(event, context):
+
+  # new order created event detail
+  eventDetail  = {
+    "orangeId": "456",
+    "orangeDate": "2020-12-10T22:01:02Z",
+    "orangeFoo": "bar",
+  }
+
+  try:
+    # Put an event
+    response = events.put_events(
+        Entries=[
+            {
+                'EventBusName': EVENT_BUS_ARN,
+                'Source': 'com.exampleCorp.OrangeService',
+                'DetailType': 'Event3',
+                'Detail': json.dumps(eventDetail)
+            }
+        ]
+    )
+    print(response['Entries'])
+    print(f"Event sent to the event bus {EVENT_BUS_ARN}")
+    print(f"EventID is {response['Entries'][0]['EventId']}")
+  except Exception as e:
+      print(e)
